eea93568683ac5b23adddf531c961310d38ed473
Andrei Gherghescu <8067229+andrei-ng@users.noreply.github.com>
2023-05-30 12:50:06 +0200

    refactor: add new example on multi-threaded serialport device
     - moves ublox_cli to examples folder

--- examples/send-receive-multiple-threads/src/main.rs
+++ examples/send-receive-multiple-threads/src/main.rs
@@ -5,86 +5,14 @@ use serialport::{
     StopBits as SerialStopBits,
 };
 use std::convert::TryInto;
+use std::thread;
 use std::time::Duration;
 use ublox::*;
 
-struct Device {
-    port: Box<dyn serialport::SerialPort>,
-    parser: Parser<Vec<u8>>,
-}
-
-impl Device {
-    pub fn new(port: Box<dyn serialport::SerialPort>) -> Device {
-        let parser = Parser::default();
-        Device { port, parser }
-    }
-
-    pub fn write_all(&mut self, data: &[u8]) -> std::io::Result<()> {
-        self.port.write_all(data)
-    }
-
-    pub fn update<T: FnMut(PacketRef)>(&mut self, mut cb: T) -> std::io::Result<()> {
-        loop {
-            let mut local_buf = [0; 100];
-            let nbytes = self.read_port(&mut local_buf)?;
-            if nbytes == 0 {
-                break;
-            }
-
-            // parser.consume adds the buffer to its internal buffer, and
-            // returns an iterator-like object we can use to process the packets
-            let mut it = self.parser.consume(&local_buf[..nbytes]);
-            loop {
-                match it.next() {
-                    Some(Ok(packet)) => {
-                        cb(packet);
-                    }
-                    Some(Err(_)) => {
-                        // Received a malformed packet, ignore it
-                    }
-                    None => {
-                        // We've eaten all the packets we have
-                        break;
-                    }
-                }
-            }
-        }
-        Ok(())
-    }
-
-    pub fn wait_for_ack<T: UbxPacketMeta>(&mut self) -> std::io::Result<()> {
-        let mut found_packet = false;
-        while !found_packet {
-            self.update(|packet| {
-                if let PacketRef::AckAck(ack) = packet {
-                    if ack.class() == T::CLASS && ack.msg_id() == T::ID {
-                        found_packet = true;
-                    }
-                }
-            })?;
-        }
-        Ok(())
-    }
-
-    /// Reads the serial port, converting timeouts into "no data received"
-    fn read_port(&mut self, output: &mut [u8]) -> std::io::Result<usize> {
-        match self.port.read(output) {
-            Ok(b) => Ok(b),
-            Err(e) => {
-                if e.kind() == std::io::ErrorKind::TimedOut {
-                    Ok(0)
-                } else {
-                    Err(e)
-                }
-            }
-        }
-    }
-}
-
 fn main() {
-    let matches = Command::new("ublox CLI example program")
+    let matches = Command::new("uBlox multi-threaded CLI example program")
         .author(clap::crate_authors!())
-        .about("Demonstrates usage of the Rust ublox API")
+        .about("Demonstrates usage of the Rust uBlox API with one thread for receiving and one for sending UBX messages.")
         .arg_required_else_help(true)
         .arg(
             Arg::new("port")
@@ -146,53 +74,51 @@ fn main() {
         eprintln!("Failed to open \"{}\". Error: {}", port, e);
         ::std::process::exit(1);
     });
+
+    // Clone the port
+    let serialport_clone = port.try_clone().expect("Failed to clone");
+
     let mut device = Device::new(port);
+    let mut device_clone = Device::new(serialport_clone);
 
-    // let s = serialport::SerialPortSettings {
-    //     baud_rate: baud,
-    //     data_bits: serialport::DataBits::Eight,
-    //     flow_control: serialport::FlowControl::None,
-    //     parity: serialport::Parity::None,
-    //     stop_bits: serialport::StopBits::One,
-    //     timeout: Duration::from_millis(1),
-    // };
-    // let port = serialport::open_with_settings(port, &s).unwrap();
-    // let mut device = Device::new(port);
+    // Send out 4 bytes every second
+    thread::spawn(move || {
+        println!("Configuration thread: configuring UART1 port ...");
+        // Example:
+        // - configure the device UART1 to talk UBX with baud rate from CLI input
+        device_clone
+            .write_all(
+                &CfgPrtUartBuilder {
+                    portid: UartPortId::Uart1,
+                    reserved0: 0,
+                    tx_ready: 0,
+                    mode: UartMode::new(DataBits::Eight, Parity::None, StopBits::One),
+                    baud_rate: baud,
+                    in_proto_mask: InProtoMask::UBLOX,
+                    out_proto_mask: OutProtoMask::union(OutProtoMask::NMEA, OutProtoMask::UBLOX),
+                    flags: 0,
+                    reserved5: 0,
+                }
+                .into_packet_bytes(),
+            )
+            .unwrap();
 
-    // Configure the device to talk UBX
-    device
-        .write_all(
-            &CfgPrtUartBuilder {
-                portid: UartPortId::Uart1,
-                reserved0: 0,
-                tx_ready: 0,
-                mode: UartMode::new(DataBits::Eight, Parity::None, StopBits::One),
-                baud_rate: 9600,
-                in_proto_mask: InProtoMask::all(),
-                out_proto_mask: OutProtoMask::UBLOX,
-                flags: 0,
-                reserved5: 0,
-            }
-            .into_packet_bytes(),
-        )
-        .unwrap();
-    device.wait_for_ack::<CfgPrtUart>().unwrap();
-
-    // Enable the NavPvt packet
-    device
-        .write_all(
-            &CfgMsgAllPortsBuilder::set_rate_for::<NavPvt>([0, 1, 0, 0, 0, 0]).into_packet_bytes(),
-        )
-        .unwrap();
-    device.wait_for_ack::<CfgMsgAllPorts>().unwrap();
-
-    // Send a packet request for the MonVer packet
-    device
-        .write_all(&UbxPacketRequest::request_for::<MonVer>().into_packet_bytes())
-        .unwrap();
+        loop {
+            println!(
+                "Configuration thread: send request for UBX-ESF-RAW and UBX-MON-VER message  ..."
+            );
+            device_clone
+                .write_all(&UbxPacketRequest::request_for::<MonVer>().into_packet_bytes())
+                .expect("Unable to write request/poll for MonVer message");
+            device_clone
+                .write_all(&UbxPacketRequest::request_for::<EsfRaw>().into_packet_bytes())
+                .expect("Unable to write request/poll for EsfRaw message");
+            thread::sleep(Duration::from_millis(1000));
+        }
+    });
 
     // Start reading data
-    println!("Opened u-blox device, waiting for solutions...");
+    println!("Opened uBlox device, waiting for messages...");
     loop {
         device
             .update(|packet| match packet {
@@ -230,10 +156,73 @@ fn main() {
                         println!("Time: {:?}", time);
                     }
                 }
+                PacketRef::EsfRaw(raw) => {
+                    println!("Got raw message: {:?}", raw);
+                }
                 _ => {
-                    println!("{:?}", packet);
+                    {}
+                    // println!("{:?}", packet);
                 }
             })
             .unwrap();
+    }
+}
+
+struct Device {
+    port: Box<dyn serialport::SerialPort>,
+    parser: Parser<Vec<u8>>,
+}
+
+impl Device {
+    pub fn new(port: Box<dyn serialport::SerialPort>) -> Device {
+        let parser = Parser::default();
+        Device { port, parser }
+    }
+
+    pub fn write_all(&mut self, data: &[u8]) -> std::io::Result<()> {
+        self.port.write_all(data)
+    }
+
+    pub fn update<T: FnMut(PacketRef)>(&mut self, mut cb: T) -> std::io::Result<()> {
+        loop {
+            let mut local_buf = [0; 100];
+            let nbytes = self.read_port(&mut local_buf)?;
+            if nbytes == 0 {
+                break;
+            }
+
+            // parser.consume adds the buffer to its internal buffer, and
+            // returns an iterator-like object we can use to process the packets
+            let mut it = self.parser.consume(&local_buf[..nbytes]);
+            loop {
+                match it.next() {
+                    Some(Ok(packet)) => {
+                        cb(packet);
+                    }
+                    Some(Err(_)) => {
+                        // Received a malformed packet, ignore it
+                    }
+                    None => {
+                        // We've eaten all the packets we have
+                        break;
+                    }
+                }
+            }
+        }
+        Ok(())
+    }
+
+    /// Reads the serial port, converting timeouts into "no data received"
+    fn read_port(&mut self, output: &mut [u8]) -> std::io::Result<usize> {
+        match self.port.read(output) {
+            Ok(b) => Ok(b),
+            Err(e) => {
+                if e.kind() == std::io::ErrorKind::TimedOut {
+                    Ok(0)
+                } else {
+                    Err(e)
+                }
+            }
+        }
     }
 }
